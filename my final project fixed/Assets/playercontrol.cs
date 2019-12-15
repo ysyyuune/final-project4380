557bbee94683457a87a01315dc140a25ef665433
Siyu Yao <43591415+ysyyuune@users.noreply.github.com>
2019-12-14 18:20:47 -0700

    project

---
+++ my final project fixed/Assets/playercontrol.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontrol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 3.5f;
+    public float gravity = 10f;
+    private RaycastHit _hit;
+    public GameObject lefthand;
+    public GameObject righthand;
+    public GameObject reaction;
+    public GameObject camera;
+    private int targetLayer = 1 << 8;
+
+    private CharacterController controller;
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    private GameObject Target;
+    void Update()
+    {
+        if(Physics.Raycast(camera.transform.position, camera.transform.forward, out _hit, Mathf.Infinity, targetLayer))
+        {
+            Target = _hit.collider.gameObject;
+            if (Input.GetButtonDown("Jump")&& lefthand.transform.childCount == 0)
+            {
+                //position of object change
+                //gameobject tag change
+                Target.gameObject.transform.parent = lefthand.transform;
+                Target.transform.localPosition = new Vector3(-1f, 2f, 1f);
+            }
+            if (Input.GetButtonDown("Fire3") && righthand.transform.childCount == 0)
+            {
+                //position of object change
+                //gameobject tag change
+                Target.transform.parent = righthand.transform;
+                Target.transform.localPosition = new Vector3(-2f, 3f, 1f);
+            }
+            if (Input.GetButtonDown("Fire1"))
+            {
+                reaction.GetComponent<reaction>();
+            }
+        }
+        PlayerMovement();
+    }
+    void PlayerMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        Vector3 velocity = direction * speed;
+        velocity.y -= gravity;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ my final project fixed/Assets/reaction.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reaction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject lefthand;
+    public TextMesh rText;
+    public GameObject righthand;
+    AudioSource audioData;
+
+    void Start()
+    {
+        audioData = lefthand.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lefthand.transform.childCount > 0 && righthand.transform.childCount>0)
+        {
+            if (lefthand.transform.Find("hydrochloric acid") && righthand.transform.Find("marble"))
+            {
+                rText.text = "2HCI+CaCO3 =>CaCl2+H2O+CO2";
+                audioData.Play(0);
+                if(Input.GetButtonDown("Fire1"))
+                {
+                    GameObject.Destroy(lefthand.transform.GetChild(0).gameObject);
+                    GameObject.Destroy(righthand.transform.GetChild(0).gameObject);
+                }
+            }
+            else if(righthand.transform.Find("hydrochloric acid") && lefthand.transform.Find("marble"))
+            {
+                rText.text = "2HCI+CaCO3 =>CaCl2+H2O+CO2";
+                audioData.Play(0);
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    GameObject.Destroy(lefthand.transform.GetChild(0).gameObject);
+                    GameObject.Destroy(righthand.transform.GetChild(0).gameObject);
+                }
+            }
+        }
+        else
+        {
+            rText.text = "you don't have enough chemicals";
+        }
+    }
+}
